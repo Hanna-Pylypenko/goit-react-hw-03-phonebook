2b9f09dd60fba4ee5171412fb43e0a40f899e911
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-07-25 13:17:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ export class App extends Component {
   }
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse('contacts');
+    const parsedContacts = JSON.parse(contacts);
     this.setState({ contacts: parsedContacts });
   }
   onFilterHandler = evt => {
